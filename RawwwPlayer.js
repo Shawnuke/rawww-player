7c02044bd8f16711a788597627e921bcaa0c264e
Shawnuke <corentin.boulanouar@hetic.net>
2021-11-29 21:19:54 +0100

    Check if the browser supports the video element

--- RawwwPlayer.js
+++ RawwwPlayer.js
@@ -3,7 +3,14 @@
 class RawwwPlayer extends HTMLElement {
 	constructor() {
 		super()
-		console.log('CustomElement connected')
+		
+		// Check if the browser actually supports the video element
+        // example: Opera Mini supports JS but not <video> element
+        const supportsVideo = !!document.createElement('video').canPlayType
+        if (!supportsVideo) return
+
+        this.$video = this.querySelector('video')
+        this.$video.controls = false
 	}
 }
 customElements.define('rawww-player', RawwwPlayer)
